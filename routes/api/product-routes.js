1deda8ccd07831ad0f7b35b746aa7e59c8e2631f
Nazia R <nazia.huma@gmail.com>
2021-06-09 18:20:22 +0100

    added post request to product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,39 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    let allProduct = await Product.findAll({
+      include: [Category] && [Tag],
+    });
+    res.json(allProduct);
+  } catch (error) {
+    console.log("not working", error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const { id } = req.params;
+    console.log(id)
+    const oneProduct = await Product.findOne(
+      { where: { id: id }, include: [Category, Tag] }
+    );
+    res.json(oneProduct);
+  } catch (error) {
+    console.log("not working", error)
+  }
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,7 +42,9 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+   
   Product.create(req.body)
+
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -48,7 +67,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,7 +108,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
 });
 
